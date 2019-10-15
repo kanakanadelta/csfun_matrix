588603a952cc2c50aee5569305186886ae8547f8
Rafael Tongson <rtongson@peppymiint.local>
2019-10-14 18:45:03 -0700

    write function

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[10,10];
+
+            for(int row = 0; row < 10; row++)
+            {
+                int rowPlace = row*10;
+                Console.Write("[");
+                for(int col = 0; col < 10; col++)
+                {
+                    matrix[row,col] = rowPlace+(col+1);
+                    Console.Write(string.Format("{0} ", matrix[row, col]));
+                }
+                Console.WriteLine("]");
+            }
+        }
+    }
+}
